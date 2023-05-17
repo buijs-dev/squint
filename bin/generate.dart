76b877c9b62c276f0fd9177a7967284ba979cac8
buijs <jjgbuijs@gmail.com>
2023-05-17 21:10:26 +0200

    Bump version to 0.1.1.

--- bin/generate.dart
+++ bin/generate.dart
@@ -26,7 +26,7 @@ import "package:squint_json/src/common/common.dart";
 Future<void> main(List<String> args) async {
   """
   ════════════════════════════════════════════
-     SQUINT (v0.0.6)                               
+     SQUINT (v0.1.1)                               
   ════════════════════════════════════════════
   """
       .log();
